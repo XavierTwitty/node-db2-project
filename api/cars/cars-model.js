14ce46b86c8bb1f33bc9d9b1614f098d15167d28
XavierTwitty <x.twitty@gmail.com>
2022-03-22 15:35:14 -0400

    get/getbyId

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,25 @@
+const db = require('../../data/db-config')
+
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars');
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  return db('cars').where('cars_id', id).first();
 }
 
 const create = () => {
   // DO YOUR MAGIC
+}
+
+const getByVin = async (vin) => {
+  return db('cars').where('vin', vin).first();
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  getByVin
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,3 +1,31 @@
+const Car = require('./cars-model');
 const router = require('express').Router();
+const {checkCarId, 
+    checkCarPayload,
+    checkVinNumberUnique,
+    checkVinNumberValid } = require("./cars-middleware")
 
+    router.get('/', (req, res) => {
+        Car.getAll()
+          .then(cars => {
+            res.json(cars);
+          })
+          .catch(err => {
+            res.status(500).json({ 
+                message: 'Failed to retrieve cars' ,
+                err: err.message
+      
+              });
+          });
+      }) 
+      
+      router.get("/:id", checkCarId, async (req, res , next) => {
+        try {
+          const car = await Car.getById(req.params.id);
+          res.status(200).json(car);
+        } catch (err) {
+          next(err);
+        }
+      });
+      
 module.exports = router
