fb64d81feb5114c461cde6e2e9b51c6d91b97c76
XavierTwitty <x.twitty@gmail.com>
2022-03-22 15:00:15 -0400

    middleware

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,3 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,25 @@
-const express = require("express")
-
+const express = require("express");
+const { logger } = require("./cars/cars-middleware");
+const carsRouter = require('./cars/cars-router');
 const server = express()
 
-// DO YOUR MAGIC
+server.use(express.json());
+server.use('/api/cars', logger,carsRouter);
+
+server.get("/", (req, res) => {
+    res.send("<h2> building a api <h2/>");
+  });
+
+  server.use('*', (req, res) => {
+    // catch all 404 errors middleware
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
+  });
+
+  server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500). json({
+      message: err.message,
+      stack: err.stack
+    })
+  })
 
 module.exports = server
